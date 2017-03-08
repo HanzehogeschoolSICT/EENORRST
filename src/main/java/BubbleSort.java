dc6923073bb312c05bd56af6b28ec5faa57a1c24
Georg Groenendaal <georg.groenendaal@gmail.com>
2017-03-08 13:15:12 +0100

    First BubbleSort implementation

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,15 +1,37 @@
 import java.util.List;
 
 /**
- * Created by georg on 3/8/17.
+ * Contains a bubble sort implementation
  */
 public class BubbleSort extends AbstractSort {
 
+    /**
+     * Bubble sort implementation
+     * @param list to be sorted
+     * @return sorted list in Ascending order
+     */
     @Override
-    public <E extends Comparable<E>> List<E> sort(List<E> list){
+    public List<Integer> sortAsc(List<Integer> list){
 
+        int listSize = list.size();
+
+        while(listSize > 1){
+            for(int i = 0; i < (listSize-1); i++){
+
+                Integer firstValue = list.get(i);
+                Integer secondValue = list.get(i+1);
+
+                // If first is bigger than second, flip the values
+                if (firstValue > secondValue){
+                    list.set(i+1, firstValue);
+                    list.set(i, secondValue);
+                }
+            }
+            listSize--;
+        }
+        return list;
     }
-    
+
     @Override
     public void step() {
         return;
